0b589347986e475ab5ac3ccecce246e1df818dd2
Saikal Bakirova <sddbakirova@gmail.com>
2022-05-12 12:15:41 +0600

    Урок 7 : fetch breeds and display on the page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [dogs, setDogs] = useState([]);
+
+  const fetchData = async () => {
+    await fetch("https://api.thedogapi.com/v1/breeds?limit=10")
+      .then((res) => res.json())
+      .then((data) => setDogs(data));
+  };
+
+  console.log("DOGS", dogs);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Dogs App</h1>
+      <button onClick={fetchData}>Get data</button>
+      <div>
+        {dogs.map((dog) => (
+          <div>
+            <img src={dog.image.url} width={300} height={300} />
+            <h1>{dog.name}</h1>
+            <ul>
+              {dog.breed_group && <li>{dog.breed_group}</li>}
+              {dog.bred_for && <li>{dog.bred_for}</li>}
+              {dog.origin ? <li>{dog.origin}</li> : null}
+              <li>{dog.life_span}</li>
+            </ul>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
